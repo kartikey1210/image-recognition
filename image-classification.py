5d58f3705c4ee2cd97cfcf915d652dc657007332
Kartikey <kartikeym95@gmail.com>
2018-10-03 23:19:02 +0530

    address of sample data changed

--- image-classification.py
+++ image-classification.py
@@ -58,7 +58,7 @@ classifier.fit_generator(
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load('/C:/Users/dps17/Desktop/kartikey/sampleData.jpg', target_size = (64,64))
+test_image = image.load('sampleData.jpg', target_size = (64,64))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 result = classifier.predict(test_image)
